c93987c24ca1b62ee4c8307958e7530f6f2600d6
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-01-12 16:04:58 +0000

    adding methods to post process results

---
+++ src/pheval_template/post_process/post_process_results_format.py
@@ -0,0 +1,52 @@
+import json
+from pathlib import Path
+from typing import List
+
+from pheval.post_processing.post_processing import PhEvalGeneResult, generate_pheval_result
+from pheval.utils.file_utils import all_files
+from pheval.utils.phenopacket_utils import GeneIdentifierUpdater, create_hgnc_dict
+
+
+def read_raw_result(raw_result_path: Path) -> List[dict]:
+    """Load Exomiser json result."""
+    with open(raw_result_path) as raw_result:
+        raw_result_data = json.load(raw_result)
+    raw_result.close()
+    return raw_result_data
+
+
+class ConvertToPhEvalResult:
+    def __init__(self, raw_result: List[dict], gene_identifier_updator: GeneIdentifierUpdater):
+        self.raw_result = raw_result
+        self.gene_identifier_updator = gene_identifier_updator
+
+    @staticmethod
+    def _obtain_score(result_entry: dict) -> float:
+        return result_entry['score']
+
+    @staticmethod
+    def _obtain_gene_symbol(result_entry: dict) -> str:
+        return result_entry['gene_symbol']
+
+    def obtain_gene_identifier(self, result_entry: dict) -> str:
+        return self.gene_identifier_updator.find_identifier(self._obtain_gene_symbol(result_entry))
+
+    def extract_pheval_gene_requirements(self):
+        pheval_result = []
+        for result_entry in self.raw_result:
+            pheval_result.append(PhEvalGeneResult(gene_symbol=self._obtain_gene_symbol(result_entry),
+                                                  gene_identifier=self.obtain_gene_identifier(result_entry),
+                                                  score=self._obtain_score(result_entry)))
+        return pheval_result
+
+
+def create_standardised_results(raw_results_dir: Path, output_dir: Path) -> None:
+    gene_identifier_updator = GeneIdentifierUpdater(gene_identifier="ensembl_id", hgnc_data=create_hgnc_dict())
+    for raw_result_path in all_files(raw_results_dir):
+        raw_result = read_raw_result(raw_result_path)
+        pheval_result = ConvertToPhEvalResult(raw_result, gene_identifier_updator).extract_pheval_gene_requirements()
+        generate_pheval_result(pheval_result=pheval_result,
+                               sort_order_str="DESCENDING",
+                               output_dir=output_dir,
+                               tool_result_path=raw_result_path)
+
