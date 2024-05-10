76bfa5209e8227efa40e2eb02c15ababbbfca742
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-05-10 20:35:48 +0100

    create hpo_ids directory

--- src/pheval_ai_marrvel/prepare/prepare.py
+++ src/pheval_ai_marrvel/prepare/prepare.py
@@ -4,4 +4,5 @@ from pheval_ai_marrvel.prepare.prepare_input import write_input_txt_files
 
 
 def prepare_inputs(testdata_dir: Path) -> None:
+    testdata_dir.joinpath("hpo_ids").mkdir(exist_ok=True)
     write_input_txt_files(testdata_dir)
