fa5eb188c6b386905e895a831e164fe32653db90
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-05-11 13:55:09 +0100

    rename command script

--- src/pheval_ai_marrvel/run/run.py
+++ src/pheval_ai_marrvel/run/run.py
@@ -8,7 +8,7 @@ def run_commands(
     tool_input_commands_dir: Path, testdata_dir: Path, input_dir: Path, output_dir
 ) -> None:
     create_apptainer_commands(tool_input_commands_dir, testdata_dir, input_dir, output_dir)
-    batch_file = tool_input_commands_dir.joinpath("apptainer_commands.txt")
+    batch_file = tool_input_commands_dir.joinpath(f"{testdata_dir.name}_commands.txt")
     subprocess.run(
         ["bash", str(batch_file)],
         shell=False,
