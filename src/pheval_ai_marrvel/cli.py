3c65cd0c273104ca795282eccc98784d33c3c265
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-05-11 13:54:44 +0100

    add post process command

---
+++ src/pheval_ai_marrvel/cli.py
@@ -0,0 +1,14 @@
+import click
+
+from pheval_ai_marrvel.post_process.post_process import post_process
+
+
+@click.group()
+def main():
+    """Exomiser runner."""
+
+
+main.add_command(post_process)
+
+if __name__ == "__main__":
+    main()
--- src/pheval_ai_marrvel/post_process/post_process.py
+++ src/pheval_ai_marrvel/post_process/post_process.py
@@ -1,7 +1,24 @@
 from pathlib import Path
 
+import click
+
 from pheval_ai_marrvel.post_process.post_process_results_format import create_standardised_results
 
 
 def post_process_results(raw_results_dir: Path, output_dir: Path) -> None:
     create_standardised_results(raw_results_dir, output_dir)
+
+
+@click.command()
+@click.option(
+    "--raw_results_dir",
+    "-r",
+    type=Path,
+)
+@click.option(
+    "--output_dir",
+    "-r",
+    type=Path,
+)
+def post_process(raw_results_dir: Path, output_dir: Path) -> None:
+    post_process_results(raw_results_dir, output_dir)
