5815472275dae8e25e2b3af8b75db30112aba838
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-06-02 14:12:05 +0200

    add functionality for running with docker

--- src/pheval_ai_marrvel/run/run.py
+++ src/pheval_ai_marrvel/run/run.py
@@ -2,10 +2,15 @@ import subprocess
 from pathlib import Path
 
 from pheval_ai_marrvel.run.create_apptainer_commands import create_apptainer_commands
+from pheval_ai_marrvel.run.create_docker_commands import run_docker
 
 
 def run_commands(
-    tool_input_commands_dir: Path, testdata_dir: Path, input_dir: Path, output_dir: Path
+    tool_input_commands_dir: Path,
+    testdata_dir: Path,
+    input_dir: Path,
+    output_dir: Path,
+    environment: str,
 ) -> None:
     """
     Run the apptainer commands.
@@ -16,9 +21,12 @@ def run_commands(
         input_dir (Path): Path to the input directory.
         output_dir (Path): Path to the output directory.
     """
-    create_apptainer_commands(tool_input_commands_dir, testdata_dir, input_dir, output_dir)
-    batch_file = tool_input_commands_dir.joinpath(f"{testdata_dir.name}_commands.txt")
-    subprocess.run(
-        ["bash", str(batch_file)],
-        shell=False,
-    )
+    if environment == "apptainer":
+        create_apptainer_commands(tool_input_commands_dir, testdata_dir, input_dir, output_dir)
+        batch_file = tool_input_commands_dir.joinpath(f"{testdata_dir.name}_commands.txt")
+        subprocess.run(
+            ["bash", str(batch_file)],
+            shell=False,
+        )
+    elif environment == "docker":
+        run_docker(testdata_dir, input_dir, output_dir)
